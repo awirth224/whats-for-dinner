c1f52a4dd432330220d9d0b63d245fd0539527a6
Angela Wirth <angiewirth92@gmail.com>
2022-07-18 18:38:39 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,51 @@
+//Arrays
+sideDishes = [
+    'Miso Glazed Carrots',
+    'Coleslaw',
+    'Garden Salad',
+    'Crispy Potatoes',
+    'Coconut Rice',
+    'Garlic Butter Mushrooms',
+    'Lemon Pepper Asparagus',    
+    'Caesar Salad',    
+    'Redskin Mashed Potatoes',
+    'Green bean casserole'
+]
+mainDishes = [
+    'Margarita Pizza',
+    'Tofu Fried Rice',
+    'Bell Pepper Fajitas',
+    'BBQ Impossible Burgers',
+    'Eggplant Parmesan',
+    'Seitan Wings',
+    'Thai Yellow Curry',
+    'Poke Bowls',
+    'Vegetarian Enchiladas',
+    'Chickpea Salad Sandwiches'
+]
+dessertDishes = [
+    'Apple Pie',
+    'Banana Bread',
+    'Cheesecake',
+    'Chocolate Cupcakes',
+    'Eclairs',
+    'Flan',
+    'Key Lime Pie',
+    'Lemon Meringue Pie',
+    'Macarons',
+    'Pumpkin pie'
+]
+
+//querySelectors
+// var  = document.querySelector('')
+// var  = documnet.querySelector('')
+// var  = document.querySelector('')
+
+//Buttons
+//letsCookButton.addEventListener
+//clearButton.addEventListener
+
+//Functions
+// entireMeal() {
+//  
+//}
